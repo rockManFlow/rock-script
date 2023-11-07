c558ecc8222f6746c2493fe4e0d2f2fabbc79801
qingyuan.cao@opay-inc.com <qingyuan.cao@opay-inc.com>
2023-11-07 11:42:28 +0800

    shell脚本实践写监控脚本+配置定时

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # rock-script
 脚本语言或脚本相关知识
+
+
+---------
+### shell脚本
+#### 定时书写
+>定时任务在Linux和Mac中使用crontab进行脚本的配置  
+>Linux和Mac的用法基本一致  
+>常用命令：  
+>crontab -l 查看当前定时任务  
+>crontab -e 编辑定时任务  
+>crontab -r 删除定时任务  
+>
+>crontab命令格式与cron格式有点区别，从分开始  
+>*　　*　　*　　*　　*　　command  
+>分  时　 日　 月　 周　    命令  
+> 在文件中直接输入 0/1 * * * * ? sh /Users/opayc/products/rock-script/com/rock/script/shell/monitor.sh /Users/opayc/products/rock-script/com/rock/script/shell/monitor.log
+
+---------
--- com/rock/script/shell/monitor.sh
+++ com/rock/script/shell/monitor.sh
@@ -1,27 +1,43 @@
 #!/bin/sh
-#定时任务在Linux和Mac中使用crontab进行脚本的配置
-#
+
+file_url=""
 #磁盘占用率报警统计-OK
 disk(){
+  file_url=$1
   #通过awk进行Linux命令报文的格式化，并把输出的xx%结果使用sed把%去掉，单输出数值
   used_value=`df -h |awk -F ' ' '/\/dev\/disk3s5/{print $5}'|sed 's/%$//'`
   if [ -n "${used_value}" ];then
-    echo "used_value:${used_value}"
+    msg="used_value:${used_value}"
+    write_file "${msg}"
     if [ ${used_value} -gt 80 ];then
-      echo "disk used over 80%.current ${used_value}%"
+      msg="disk used over 80%.current ${used_value}%"
+      write_file "${msg}"
     elif [ ${used_value} -gt 50 ]; then
-      echo "disk used over 50%.current ${used_value}%"
+      msg="disk used over 50%.current ${used_value}%"
+      write_file "${msg}"
     elif [ ${used_value} -gt 20 ]; then
-         echo "disk used over 20%.current ${used_value}%"
+      msg="disk used over 20%.current $used_value%"
+      write_file "${msg}"
     else
-         echo "disk used  ${used_value}"
+      msg="disk used  ${used_value}"
+      write_file "${msg}"
     fi
   else
-      echo "run error"
+      msg="run error"
+      write_file "${msg}"
   fi
 }
 
-#触发调用
-disk
+#写信息追加到文件中
+write_file(){
+  date1=`date +"%Y-%m-%d %H:%M:%S"`
+  msg="${date1} $1 "
+  echo ${msg} >> ${file_url}
+}
 
-#0 0/1 * * * ? sh /Users/opayc/products/rock-script/com/rock/script/shell/monitor.sh > /Users/opayc/products/rock-script/com/rock/script/shell/monitor.log
+#触发调用
+disk $1
+
+
+#触发命令：sh monitor.sh monitor.log
+#
